95777795a3dc7a417993be98507682e637b3f065
Ayush Agarwal <agarwal.ayush.0109@gmail.com>
2022-11-01 12:33:47 +0530

    Master logic updated

--- pattern.c
+++ pattern.c
@@ -7,5 +7,11 @@ int main() {
         }
         printf("\n");
     }
+    for(int i=1; i<=4; i++) {
+        for(int j=4; j>=i; j--) {
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
